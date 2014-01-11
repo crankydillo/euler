21129e073290b0f8532d011cd2a8b5c9972a4405
samuel <crankydillo@gmail.com>
2014-01-11 08:54:12 -0600

    Not using Maven for this.

--- scala/Problem6.scala
+++ scala/Problem6.scala
@@ -1,12 +1,12 @@
-//package org.samuel.euler;
 
-// The sum of the squares of the first ten natural numbers is, 385
-
-// The square of the sum of the first ten natural numbers is, 3025
-
-// Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025  385 = 2640.
-
-// Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+/*
+ * The sum of the squares of the first ten natural numbers is, 385 The square of
+ * the sum of the first ten natural numbers is, 3025 Hence the difference
+ * between the sum of the squares of the first ten natural numbers and the
+ * square of the sum is 3025  385 = 2640.  Find the difference between the sum
+ * of the squares of the first one hundred natural numbers and the square of the
+ * sum.
+ */
 
 object Problem6 {
   def answer(n: Int) = squareSum(n) - sumSquares(n);
--- scala/Problem9.scala
+++ scala/Problem9.scala
@@ -1,14 +1,15 @@
-package org.samuel.euler;
 
-// A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
-// a^(2) + b^(2) = c^(2)
+/*
+ * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+ * a^(2) + b^(2) = c^(2)
 
-// From wikipedia, natural numbers = positive or non-negative integers
+ * From wikipedia, natural numbers = positive or non-negative integers
 
-// For example, 3^(2) + 4^(2) = 9 + 16 = 25 = 5^(2).
+ * For example, 3^(2) + 4^(2) = 9 + 16 = 25 = 5^(2).
 
-// There exists exactly one Pythagorean triplet for which a + b + c = 1000.
-// Find the product abc.
+ * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ * Find the product abc.
+ */
 
 object Problem9 {
 
